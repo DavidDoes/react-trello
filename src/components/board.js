ee9fdd36290d28b27be8b4634e3929dbf275c189
David Clark <dlc.clark@gmail.com>
2018-12-22 17:39:23 -0800

    add static.json

--- src/components/board.js
+++ src/components/board.js
@@ -1,51 +1,48 @@
-import React from 'react';
+import React from "react";
 
-import List from './list';
-import AddForm from './add-form';
+import List from "./list";
+import AddForm from "./add-form";
 
-import './board.css';
+import "./board.css";
 
 export default class Board extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            lists: []
-        };
+    this.state = {
+      lists: []
+    };
 
-        this.addList = this.addList.bind(this);
-    }
+    this.addList = this.addList.bind(this);
+  }
 
-    addList(title) {
-        this.setState({
-            lists: [...this.state.lists, {title}]
-        });
-    }
+  addList(title) {
+    this.setState({
+      lists: [...this.state.lists, { title }]
+    });
+  }
 
-    render() {
-        const lists = this.state.lists.map((list, index) => (
-            <li className="list-wrapper" key={index}>
-                <List {...list} />
-            </li>
-        ));
+  render() {
+    const lists = this.state.lists.map((list, index) => (
+      <li className="list-wrapper" key={index}>
+        <List {...list} />
+      </li>
+    ));
 
-        return (
-            <div className="board">
-                <h2>{this.props.title}</h2>
-                <ul className="lists">
-                    {lists}
-                    <li className="add-list-wrapper">
-                        <AddForm
-                            type="list"
-                            onAdd={text => this.addList(text)}
-                        />
-                    </li>
-                </ul>
-            </div>
-        );
-    }
+    return (
+      <div className="board">
+        <h2>{this.props.title}</h2>
+        <ul className="lists">
+          {lists}
+          <li className="add-list-wrapper">
+            <AddForm type="list" onAdd={text => this.addList(text)} />
+          </li>
+        </ul>
+      </div>
+    );
+  }
 }
 
 Board.defaultProps = {
-    title: 'Board'
+  title: "Board"
 };
